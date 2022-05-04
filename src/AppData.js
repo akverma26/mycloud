96ed747a29df3fde6c4583a758e4ce75c656ec4e
Ankit Kumar <akvindian26@gmail.com>
2022-05-04 20:22:29 +0530

    build added

--- src/AppData.js
+++
@@ -1,8 +0,0 @@
-const APP_DATA = {
-    APP_NAME: "MyCloud",
-    VERSION: "1.0.1-alpha",
-    // SERVER_URL: "http://127.0.0.1:8000/",
-    SERVER_URL: "https://akvcloud.pythonanywhere.com/",
-};
-
-export default APP_DATA;
--- src/static/js/backendConnection.js
+++
@@ -1,8 +0,0 @@
-import APP_DATA from "./../../AppData";
-
-const SERVER_URL = APP_DATA.SERVER_URL;
-
-export default function fetchForServer(path, init = {}, relativePath = true) {
-    if (relativePath) path = new URL(path, SERVER_URL);
-    return fetch(path, init);
-}
